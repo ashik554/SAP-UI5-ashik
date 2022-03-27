2e57eba4b8c5bcfa0261ce8abcd21849be9c137a
Ashikur Rahman <ashikurrahman@MacBook-Air.local>
2022-03-27 07:24:51 +0200

    up

--- controller/App.controller.js
+++ controller/App.controller.js
@@ -1,13 +1,7 @@
-sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function(
-	Controller
-) {
-	"use strict";
+sap.ui.define(['sap/ui/core/mvc/Controller'], function (Controller) {
+  'use strict';
 
-	return Controller.extend("emc.hr.payroll.App", {
-       onInit:function () {
-           
-       } 
-	});
+  return Controller.extend('emc.hr.payroll.controller.App', {
+    onInit: function () {},
+  });
 });
